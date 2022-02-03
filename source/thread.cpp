65aebfc6130359a634d36b69ebec6c55bc238097
Shrujal123 <shrujal@Shrujals-PC.localdomain>
2022-02-02 17:01:19 -0800

    test commit

--- source/thread.cpp
+++ source/thread.cpp
@@ -14,18 +14,18 @@ void print_thread_id (int id)
 	mtx.lock();
 	cout << "thread #" << id << endl;
 	mtx.unlock();
-
+	cout << "development branch comment" << endl;
 }
 
 int main ()
 {
 
-	MPI_INIT(NULL, NULL);
-	int world_rank;
-	int world_size;
+	//MPI_INIT(NULL, NULL);
+	//int world_rank;
+	//int world_size;
 
-	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+	//MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+	//MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
 	
 
